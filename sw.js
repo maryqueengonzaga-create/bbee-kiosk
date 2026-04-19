e9c068eaa6305e61a1455a22f8c85270bb199839
maryqueengonzaga-create <maryqueen.gonzaga@gmail.com>
2026-04-20 01:08:59 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { clients.claim(); });
-self.addEventListener('fetch', e => {
-  const url = e.request.url;
-  if (url.includes('script.google.com') || url.includes('cloudinary.com') || url.includes('fonts.googleapis.com') || url.includes('fonts.gstatic.com')) return;
-  e.respondWith(fetch(e.request).catch(() => new Response('')));
-});
